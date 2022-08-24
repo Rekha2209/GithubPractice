4539058402a86f62d5d5aed4536271f7e46193f9
My PC <My PC@DESKTOP-P8J02CA>
2022-08-24 17:05:47 +0530

    Practice1

---
+++ src/main/java/simpleGithub/PracticeGit.java
@@ -0,0 +1,15 @@
+package simpleGithub;
+
+import org.testng.annotations.Test;
+
+public class PracticeGit {
+	@Test
+	public void secondMethod()
+	{
+		System.out.println(" second mehtod");
+	}
+	
+	
+
+
+}
