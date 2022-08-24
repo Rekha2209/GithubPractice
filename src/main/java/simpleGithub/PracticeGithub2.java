3501a3a702a368f07e18adc770ae1e73ee655a81
Manu <Manu@Manu>
2022-08-24 17:35:01 +0530

    branch 2 added

---
+++ src/main/java/simpleGithub/PracticeGithub2.java
@@ -0,0 +1,13 @@
+package simpleGithub;
+
+import org.testng.annotations.Test;
+
+public class PracticeGithub2 {
+	
+	@Test
+	public void method()
+	{
+		System.out.println(" git second method");
+	}
+
+}
